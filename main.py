2eeea794f4fd0975218d8592356eb1a27b84521a
Santiago Goldstein <santiago3237@hotmail.com>
2024-09-06 16:51:43 -0300

    Se implemento la opcion de ingresar tareas o eventos. Se modifico la forma de eliminar eventos o tareas. Se modifico la funcion de ver registros o eventos segun corresponda

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 eventos = []
+tareas = []
 
 def insertar_eventos():
-    # Funcion que inserta eventos.
+    # Funcion que inserta eventos o Tareas.
     evento_usuario = True
     while evento_usuario:
-        print("Ingrese un nuevo Evento:")
-        evento_usuario = input("")
-        eventos.append(evento_usuario)
-        print("---------------------------------")
-        print("| Evento Guardado ¡Exitosamente!|")
-        print("---------------------------------\n")
+        print("Que quiere insertar?:")
+        print("""
+    (1) Para insertar un Evento
+    (2) Para insertar una Tarea
+    """)
+        tarea_evento = int(input())
+        if tarea_evento == 1:
+            print("Ingrese un nuevo Evento:")
+            evento_usuario = input("")
+            eventos.append(evento_usuario)
+            print("---------------------------------")
+            print("| Evento Guardado ¡Exitosamente!|")
+            print("---------------------------------\n")
+        elif tarea_evento == 2:
+            print("Ingrese una nueva Tarea:")
+            tarea_usuario = input("")
+            tareas.append(tarea_usuario)
+            print("--------------------------------")
+            print("| Tarea Guardada ¡Exitosamente!|")
+            print("--------------------------------\n")
+        else:
+            print("Por favor elija una opcion valida, Gracias")
 
-        print("Desea seguir agregando eventos? escriba 'si' o 'no'")
+        print("Desea seguir agregando eventos o tareas? escriba 'si' o 'no'")
         sigue = input("").lower()
 
         if sigue != "si":
@@ -20,19 +37,42 @@ def insertar_eventos():
 
 def eliminar_eventos():
     # Funcion que elimina registros
-    if len(eventos) != 0:
-        print(f"Eventos que puedes eliminar:")
-        for id, evento in enumerate (eventos):
-            print(f"{id + 1}. {evento}")
+    print("Que quiere Eliminar?:")
+    print("""
+    (1) Para eliminar un Evento
+    (2) Para eliminar una Tarea
+    """)
+    tarea_evento_eliminar = int(input())
+    if tarea_evento_eliminar == 1:
+        if len(eventos) != 0:
+            print(f"Eventos que puedes eliminar:")
+            for id_even, evento in enumerate (eventos):
+                print(f"{id_even + 1}. {evento}")
 
-        print("\nIngrese el numero de evento a eliminar:")
-        seleccion = int(input()) -1 # convierte en indice la seleccion con -1
-        evento_eliminado = eventos.pop(seleccion)
-        print(f"Se ha eliminado el evento {evento_eliminado}")
+            print("\nIngrese el numero de evento a eliminar:")
+            seleccion = int(input()) -1 # convierte en indice la seleccion con -1
+            evento_eliminado = eventos.pop(seleccion)
+            print(f"Se ha eliminado el evento {evento_eliminado}")
+        else:
+            print("---------------------------------")
+            print("| ¡No hay eventos que eliminar! |")
+            print("---------------------------------")
+    elif tarea_evento_eliminar == 2:
+        if len(tareas) != 0:
+            print(f"Tareas que puedes eliminar:")
+            for id_tarea, tarea in enumerate (tareas):
+                print(f"{id_tarea + 1}. {tarea}")
+
+            print("\nIngrese el numero de evento a eliminar:")
+            seleccion = int(input()) -1 # convierte en indice la seleccion con -1
+            tarea_eliminada = tareas.pop(seleccion)
+            print(f"Se ha eliminado la tarea {tarea_eliminada}")
+        else:
+            print("---------------------------------")
+            print("| ¡No hay tareas que eliminar! |")
+            print("---------------------------------")
     else:
-        print("---------------------------------")
-        print("| ¡No hay eventos que eliminar! |")
-        print("---------------------------------")
+        print("por favor elija una opcion valida, Gracias")
     
 def modificar_eventos():
     # Funcion que modifica eventos
@@ -40,14 +80,19 @@ def modificar_eventos():
 
 def ver_eventos_pendientes():
     # Funcion para ver registros en memoria
-    if len(eventos) != 0:
-        print(f"Eventos registrados:")
-        for id, evento in enumerate (eventos):
-            print(f"{id + 1}. {evento}")
+    if len(eventos) != 0 or len(tareas) != 0:
+        print(f"\nEventos registrados:")
+        for id_even, evento in enumerate (eventos):
+            print(f"{id_even + 1}. {evento}")
+        
+        print("\nTareas registradas:")
+        for id_tarea, tarea in enumerate(tareas):
+            print(f"{id_tarea +1}. {tarea}")
+    
     else:
-        print("--------------------------------")
-        print("| ¡No hay eventos registrados! |")
-        print("--------------------------------")
+        print("-----------------------------------------")
+        print("| ¡No hay eventos o tareas registrados! |")
+        print("-----------------------------------------")
 
 def main():
     # el usuario debera elegir la accion que desee.
